d08256e10b7cf58b90335418ed77fb2a86accd1d
pachungulo <115584328+pachungulo@users.noreply.github.com>
2024-10-18 20:17:35 -0400

    Changed the way color works in terminal to now use eza config
    LS_COLORS left as default, as terminal takes care of the rest

--- files/private_dot_config/fish/conf.d/misc_setup.fish
+++ files/private_dot_config/fish/conf.d/misc_setup.fish
@@ -5,3 +5,7 @@ end
 if test -n "$GHOSTTY_RESOURCES_DIR"
   builtin source {$GHOSTTY_RESOURCES_DIR}/shell-integration/fish/vendor_conf.d/ghostty-shell-integration.fish
 end
+
+if set -q LS_COLORS
+    set -e LS_COLORS
+end
