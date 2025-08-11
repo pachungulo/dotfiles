8056d66cbe6365a52c757a7fdb0117673c7be16f
pachungulo <115584328+pachungulo@users.noreply.github.com>
2025-08-10 22:32:32 -0400

    Disabled wayland for better window decorations

--- files/private_dot_config/wezterm/wezterm.lua
+++ files/private_dot_config/wezterm/wezterm.lua
@@ -16,6 +16,8 @@ config.command_palette_fg_color = "#bac2de"
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 20
 config.kde_window_background_blur = true
+config.window_decorations = "RESIZE"
+config.enable_wayland = false
 
 config.window_background_gradient = {
     orientation = "Vertical",
